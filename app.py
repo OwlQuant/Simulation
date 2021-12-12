55e31c9a40e8f9e96613e5fa6fe20e4efac216eb
OwlQuant <owlquant@gmail.com>
2021-12-12 21:59:37 +0000

    yes

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, request, jsonify
 import numpy as np
 
 # import simulation.stock as stock
-import simulation as sim
-
+import Simulation.src.simulation as sim
+# from simulation import .src.simulation import gbm
 app = Flask(__name__)
 
 @app.route('/api/gbm/v1', methods=['GET'])
